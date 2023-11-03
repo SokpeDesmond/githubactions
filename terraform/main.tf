16b91752b2b449ef39394c7acd5fdcb81933f8f8
SokpeDesmond <desmondsokpe1@gmail.com>
2023-11-03 00:35:27 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "des22" {
+resource "aws_instance" "ec2_1" {
   ami                     = "ami-0989fb15ce71ba39e"
-  instance_type           = "t2.micro"
+  instance_type           = "t3.micro"
  
 tags = {
   name = "githubactionsEc2"
