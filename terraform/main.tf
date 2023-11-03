0f98ae753c01415ba0afe5beb10df9745285c357
SokpeDesmond <desmondsokpe1@gmail.com>
2023-11-03 01:03:24 +0000

    vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,49 @@
-resource "aws_instance" "ec2_1" {
-  ami                     = "ami-0989fb15ce71ba39e"
-  instance_type           = "t3.micro"
- 
-tags = {
-  name = "githubactionsEc2"
+resource "aws_instance" "sokpe_11" {
+  ami           = var.ami
+  instance_type = var.instance_type["production"]
+  # aws_subnet =  var.aws_vpc.main.id
+  tags = {
+    name = var.servers[1]
+  }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "prisubnt_1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.prisudnet_cidr1
+
+  tags = {
+    Name = var.prisubnet_name[0]
+  }
+}
+
+resource "aws_subnet" "prisubnt_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.prisudnet_cidr2
+
+  tags = {
+    Name = var.prisubnet_name[1]
+  }
+}
+
+resource "aws_subnet" "pubsubnt" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.pubsudnet_cidr1
+ tags = {
+    Name = var.pubsubnet_name[0]
+  }
+}
+
+resource "aws_subnet" "pubsubnt2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.pubsudnet_cidr2
+  tags = {
+    Name = var.pubsubnet_name[1]
+  }
 }
